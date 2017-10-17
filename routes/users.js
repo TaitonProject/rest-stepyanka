df75b24aeda0ea31bf0a0446e43c2f70b31ccdbf
agrushkevich <taiton252005@gmail.com>
2017-10-18 01:20:29 +0200

    add headers

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
+    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header("Expires", 0);
     res.json({
         people: {
             age: 22,
